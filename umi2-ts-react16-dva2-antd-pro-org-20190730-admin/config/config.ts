a7f232d49edaf2f20e2b14079e9148a6352e80ca
luo0412 <1095847440@qq.com>
2019-07-30 17:00:27 +0800

    adjust

--- umi2-ts-react16-dva2-antd-pro-org-20190730-admin/config/config.ts
+++ umi2-ts-react16-dva2-antd-pro-org-20190730-admin/config/config.ts
@@ -1,18 +1,13 @@
 import { IConfig, IPlugin } from 'umi-types';
+import defaultSettings from './defaultSettings'; // https://umijs.org/config/
 
-import defaultSettings from './defaultSettings';
-// https://umijs.org/config/
 import slash from 'slash2';
 import webpackPlugin from './plugin.config';
-
-const { pwa, primaryColor } = defaultSettings;
-
-// preview.pro.ant.design only do not use in your production ;
+const { pwa, primaryColor } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
+
 const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
-
 const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
-
 const plugins: IPlugin[] = [
   [
     'umi-plugin-react',
@@ -41,8 +36,7 @@ const plugins: IPlugin[] = [
               importWorkboxFrom: 'local',
             },
           }
-        : false,
-      // default close dll, because issue https://github.com/ant-design/ant-design-pro/issues/4665
+        : false, // default close dll, because issue https://github.com/ant-design/ant-design-pro/issues/4665
       // dll features https://webpack.js.org/plugins/dll-plugin/
       // dll: {
       //   include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
@@ -59,9 +53,8 @@ const plugins: IPlugin[] = [
       autoAddMenu: true,
     },
   ],
-];
+]; // 针对 preview.pro.ant.design 的 GA 统计代码
 
-// 针对 preview.pro.ant.design 的 GA 统计代码
 if (isAntDesignProPreview) {
   plugins.push([
     'umi-plugin-ga',
@@ -95,6 +88,26 @@ export default {
       Routes: ['src/pages/Authorized'],
       authority: ['admin', 'user'],
       routes: [
+        {
+          name: 'account-settings',
+          path: '/account-settings',
+          component: './account-settings',
+        },
+        {
+          name: 'exception-404',
+          path: '/exception-404',
+          component: './exception-404',
+        },
+        {
+          name: 'exception-500',
+          path: '/exception-500',
+          component: './exception-500',
+        },
+        {
+          name: 'user-login',
+          path: '/user-login',
+          component: './user-login',
+        },
         {
           path: '/',
           name: 'welcome',
--- umi2-ts-react16-dva2-antd-pro-org-20190730-admin/src/locales/zh-CN/menu.ts
+++ umi2-ts-react16-dva2-antd-pro-org-20190730-admin/src/locales/zh-CN/menu.ts
@@ -41,6 +41,7 @@ export default {
   'menu.account': '个人页',
   'menu.account.center': '个人中心',
   'menu.account.settings': '个人设置',
+  'menu.account-settings': '个人设置',
   'menu.account.trigger': '触发报错',
   'menu.account.logout': '退出登录',
   'menu.editor': '图形编辑器',
